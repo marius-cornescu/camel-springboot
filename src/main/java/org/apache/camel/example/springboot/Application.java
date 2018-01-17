912369aa5263418c1b5056eaca373f62365466dd
marius_cornescu <marius_cornescu@yahoo.com>
2018-01-17 21:34:37 +0200

    INIT PROJECT

---
+++ src/main/java/org/apache/camel/example/springboot/Application.java
@@ -0,0 +1,26 @@
+package org.apache.camel.example.springboot;
+
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main method to start the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean camelServletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/camel/*");
+        registration.setName("CamelServlet");
+        return registration;
+    }
+
+}
